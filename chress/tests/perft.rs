87e9cb6257219bf3be36048f5ae02011794583a6
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-07-17 17:26:39 +0200

    update tests

--- chress/tests/perft.rs
+++ chress/tests/perft.rs
@@ -1,14 +1,14 @@
+pub const KIWIPETE: &str = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+pub const POSITION_3: &str = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+pub const POSITION_4: &str = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+pub const POSITION_5: &str = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+
 #[cfg(test)]
 pub mod perft_speed_tests {
+    use chress::{board::Board, debug::perft, move_gen::MoveGen};
     use std::time;
 
-    use chress::{board::Board, debug::perft, move_gen::MoveGen};
-
-    pub const KIWIPETE: &str =
-        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
-    pub const POSITION_3: &str = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
-    pub const POSITION_4: &str = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
-    pub const POSITION_5: &str = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+    use super::*;
 
     #[test]
     fn startpos() {
@@ -51,16 +51,45 @@ pub mod perft_speed_tests {
 pub mod perft_tests {
     use chress::{board::Board, debug::perft, move_gen::MoveGen};
 
+    use super::*;
+
     #[test]
     fn startpos() {
         let move_gen = MoveGen::new();
         let board = Board::default();
 
-        assert_eq!(perft(board, &move_gen, 0), 1);
-        assert_eq!(perft(board, &move_gen, 1), 20);
-        assert_eq!(perft(board, &move_gen, 2), 400);
-        assert_eq!(perft(board, &move_gen, 3), 8902);
-        assert_eq!(perft(board, &move_gen, 4), 197281);
         assert_eq!(perft(board, &move_gen, 5), 4865609);
+    }
+
+    #[test]
+    fn kiwipete() {
+        let move_gen = MoveGen::new();
+        let board = Board::from_fen(KIWIPETE, &move_gen).unwrap();
+
+        assert_eq!(perft(board, &move_gen, 5), 193690690);
+    }
+
+    #[test]
+    fn position_3() {
+        let move_gen = MoveGen::new();
+        let board = Board::from_fen(POSITION_3, &move_gen).unwrap();
+
+        assert_eq!(perft(board, &move_gen, 7), 178633661);
+    }
+
+    #[test]
+    fn position_4() {
+        let move_gen = MoveGen::new();
+        let board = Board::from_fen(POSITION_4, &move_gen).unwrap();
+
+        assert_eq!(perft(board, &move_gen, 5), 15833292);
+    }
+
+    #[test]
+    fn position_5() {
+        let move_gen = MoveGen::new();
+        let board = Board::from_fen(POSITION_5, &move_gen).unwrap();
+
+        assert_eq!(perft(board, &move_gen, 5), 89941194);
     }
 }
